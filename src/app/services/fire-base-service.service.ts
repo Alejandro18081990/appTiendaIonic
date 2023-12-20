66a87ce787924a2da366859d96d7a24d94fdf20a
Alejandro18081990 <alexnrdriguez@gmail.com>
2023-12-20 12:41:47 +0100

    CommitClase201223V2

---
+++ src/app/services/fire-base-service.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { Producto } from '../interfaces/producto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FireBaseServiceService {
+
+  constructor(private firestore : AngularFirestore) { }
+
+  getAll(): Observable<Producto> {
+    console.log("llamando a firebase")
+    return this.firestore.collection('Productos').valueChanges();
+  }
+}

74e1a4cc20fb2c10af627d3de67e91b72c7cf2e5
Alejandro18081990 <alexnrdriguez@gmail.com>
2023-12-20 12:32:58 +0100

    CommitClase201223FireBaseV1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { CommonModule } from '@angular/common';
 import { ComponentsModule } from './components/components.module';
+import { AngularFireModule } from "@angular/fire/compat"
+import { AngularFirestoreModule } from "@angular/fire/compat/firestore"
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, ComponentsModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, ComponentsModule,AngularFireModule.initializeApp(environment.firebaseConfig),AngularFirestoreModule ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  apiURL: "http://localhost:3000"
+  apiURL: "http://localhost:3000",
+  firebaseConfig : {
+    apiKey: "AIzaSyA4L4SUe_fCEwk78563mx-5Hj-N4PmAr-I",
+    authDomain: "notas-a44ac.firebaseapp.com",
+    projectId: "notas-a44ac",
+    storageBucket: "notas-a44ac.appspot.com",
+    messagingSenderId: "417798488904",
+    appId: "1:417798488904:web:4a8ab0bcc18d47c6e0a97d",
+    measurementId: "G-08DFH249Q0"
+  }
 };
